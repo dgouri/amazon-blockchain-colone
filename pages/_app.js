d2fc43b3f5cbb82d83b50f661c8201e587f97144
dgouri <95135687+dgouri@users.noreply.github.com>
2022-07-17 14:17:23 +0530

    first commit

---
+++ pages/_app.js
@@ -0,0 +1,22 @@
+import '../styles/globals.css'
+import {MoralisProvider} from 'react-moralis'
+import {AmazonProvider} from '../context/AmazonContext'
+
+function MyApp({ Component, pageProps }) {
+  return (
+    <MoralisProvider
+    appId="process.env.NEXT_PUBLIC_MORALIS_APP_ID"
+    serverUrl="process.env.NEXT_PUBLIC_MORALIS_SERVER"
+    
+    >
+      <AmazonProvider>
+       <Component {...pageProps} />
+      </AmazonProvider>
+    
+    </MoralisProvider>
+  )
+  
+  
+}
+
+export default MyApp
